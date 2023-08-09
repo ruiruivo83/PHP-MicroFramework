c4d68acd927c10c240d5038ab7068cc3a3115d06
Rui Ruivo <ruivo.rui@gmail.com>
2023-08-09 18:10:00 +0200

    Adding more pages

---
+++ App/Controllers/Buisness/Recruitment.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controllers\Buisness;
+
+use App\Auth;
+use \Core\View;
+
+/**
+ * Home Controller
+ * 
+ * PHP version 7.4
+ */
+class Recruitment extends \Core\Authenticated
+{
+
+    /**
+     * Before filter - called before an action method.
+     * 
+     * @return void
+     */
+    /*
+    protected function Before()
+    {
+        echo " (before in \Controllers\Home)";
+        // return false; // Will stop the execution
+    }
+    */
+
+    /**
+     * After filter - called after an action method.
+     * 
+     * @return void
+     */
+    /*
+    protected function After()
+    {
+        echo " (after in \Controllers\Home)";
+    }
+    */
+
+    /**
+     * Show the index page
+     * 
+     * @return void
+     */
+    public function indexAction()
+    {
+        View::renderTemplate('Buisness/Recruitment/index.html', [
+            'userModel' => Auth::getUser()
+        ]);
+    }
+}
