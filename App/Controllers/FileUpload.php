ad9db0e9efb1380360eb260286ba2697c854a17a
Rui Ruivo <ruivo.rui@gmail.com>
2023-09-18 20:51:58 +0200

    File Upload NOK for PHP 7.4

--- App/Controllers/FileUpload.php
+++ App/Controllers/FileUpload.php
@@ -23,28 +23,86 @@ class FileUpload extends Authenticated
     public function newAction()
     {
 
-        $uploaddir = '../data/uploads/';
-        $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+        // MODEL
+        $phpFileUploadErrors = array(
+            0 => 'There is no error, the file uploaded with success',
+            1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+            2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+            3 => 'The uploaded file was only partially uploaded',
+            4 => 'No file was uploaded',
+            6 => 'Missing a temporary folder',
+            7 => 'Failed to write file to disk.',
+            8 => 'A PHP extension stopped the file upload.',
+        );
 
-        var_dump($uploadfile);
+        // UPLOAD
+        $target_dir  = '../data/uploads/';
+        $target_file  = $target_dir  . basename($_FILES['userfile']['name']);
 
+        echo 'file count=', count($_FILES),"\n";
+        echo "File Count: " . var_dump($_FILES);
 
-        /** Get the name of the uploaded file  */
-        // $filename =$_FILES['userfile']['name']
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-        echo '<pre>';
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["userfile"]["tmp_name"]);
 
-        if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-            echo "File is valid, and was successfully uploaded.\n";
-        } else {
-            echo "Error uploading file!\n";         
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+
         }
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+
+        // Check file size
+        if ($_FILES["userfile"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" && $imageFileType != "sql" && $imageFileType != "pdf" && $imageFileType != "mov" && $imageFileType != "avi" && $imageFileType != "mp4") {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+            
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["userfile"]["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars( basename( $_FILES["userfile"]["name"])). " has been uploaded.";
+            } else {
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+
+        // MODEL
+        var_dump($_POST);
+        var_dump($_FILES['userfile']['name']);
+        var_dump("File Size: " . $_FILES['userfile']['size']);
         
+        // MODEL
         echo 'Here is some more debugging info:';
-        print_r($_FILES);        
+        print_r($_FILES);
+        echo "Error description: " . $phpFileUploadErrors[$_FILES['userfile']['error']]."\n";
+
         print "</pre>";
 
-        // View::renderTemplate('coursestudio/newchapter.html');
+        View::renderTemplate('coursestudio/newchapter.html');
 
     }
 
