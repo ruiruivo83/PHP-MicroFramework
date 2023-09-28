c5231d108c896aef2f60ebbf7e61bfc5d6132dfb
Rui Ruivo <ruivo.rui@gmail.com>
2023-09-28 18:25:32 +0200

    update

--- Database/Scripts/202309230947.sql
+++ Database/Scripts/202309230947.sql
@@ -2,6 +2,5 @@ CREATE TABLE Payments (
     id INT AUTO_INCREMENT PRIMARY KEY,
     userId INT NOT NULL,
     paymentDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    paymentExpirationDate TIMESTAMP NOT NULL,
-    FOREIGN KEY (userId) REFERENCES Users(id) -- Assuming there is a Users table with a matching id field
+    paymentExpirationDate TIMESTAMP NOT NULL   
 );
